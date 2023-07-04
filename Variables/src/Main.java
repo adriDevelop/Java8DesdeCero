402492c027e56a6d9c6379b58fa9fcd43cb0d9c1
Adrian <adridevelop11@gmail.com>
2023-07-04 09:59:11 +0200

    Comenzamos a manejar variables y tipos de datos.

---
+++ Variables/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Declaracion e inicializacion de dos variables
+        int numero = 7;
+        char letra = 'a';
+
+        // Declaracion e inicializacion de una constante
+        final int NUM_HORAS = 8;
+
+        // Impresion por consola de los tres valores
+        System.out.println(numero);
+
+        System.out.println(letra);
+
+        System.out.println(NUM_HORAS);
+    }
+}
